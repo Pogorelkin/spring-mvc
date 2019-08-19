1f53898658ee6d6123a6edaaecef2775622c16fc
Vadim <vadim_pogorelkin@epam.com>
2019-08-19 16:15:17 +0400

    ref2

--- src/main/java/com/springmvc/controllers/EmployeeControllerREST.java
+++ src/main/java/com/springmvc/controllers/EmployeeControllerREST.java
@@ -3,38 +3,40 @@ package com.springmvc.controllers;
 import com.springmvc.entities.Employee;
 import com.springmvc.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/employee")
 public class EmployeeControllerREST {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping("/employee/{employeeId}")
+    @GetMapping("/{employeeId}")
     private Employee getEmployee(@PathVariable int employeeId) {
         return employeeService.getEmployeeById(employeeId);
     }
 
-    @PostMapping("/employee")
-    private Employee addEmployee(@RequestBody Employee employee) {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    private void addEmployee(@RequestBody Employee employee) {
         employeeService.addEmployee(employee);
-        return employee;
     }
 
-    @PutMapping("/employee")
+    @PutMapping
     private void
     updateEmployee(@RequestBody Employee employee) {
         employeeService.updateEmployee(employee);
     }
 
-    @DeleteMapping("/employee/{employeeId}")
+    @DeleteMapping("/{employeeId}")
     private void deleteEmployee(@PathVariable int employeeId) {
         employeeService.deleteEmployeeById(employeeId);
     }
 
-    @GetMapping("/employee/all")
+    @GetMapping("/all")
     private List<Employee> getEmployees() {
         return employeeService.getAllEmployees();
     }
